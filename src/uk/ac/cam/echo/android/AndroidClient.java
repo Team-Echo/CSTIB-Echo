a31ba893e1ade791d60b22812c6d7283adf4ea49
Petar Veličković <pv273@cam.ac.uk>
2014-01-25 15:30:10 +0000

    Added Android and touchscreen client placeholder classes.

---
+++ src/uk/ac/cam/echo/android/AndroidClient.java
@@ -0,0 +1,8 @@
+package uk.ac.cam.echo.android;
+
+import uk.ac.cam.echo.client;
+
+public class AndroidClient extends Client
+{
+    // TODO
+}
---
+++ src/uk/ac/cam/echo/touch/TouchClient.java
@@ -0,0 +1,8 @@
+package uk.ac.cam.echo.touch;
+
+import uk.ac.cam.echo.client;
+
+public class TouchClient extends Client
+{
+    // TODO
+}
