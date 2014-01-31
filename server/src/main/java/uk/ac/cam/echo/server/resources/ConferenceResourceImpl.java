7e8ff870b98d7ed264879d8187a728fb16c79375
Alexandru Tache <hadesgames@gmail.com>
2014-01-31 20:17:56 +0000

    Some methods were missing and it would not compile

--- server/src/main/java/uk/ac/cam/echo/server/resources/ConferenceResourceImpl.java
+++ server/src/main/java/uk/ac/cam/echo/server/resources/ConferenceResourceImpl.java
@@ -1,7 +1,7 @@
 package uk.ac.cam.echo.server.resources;
 
-import org.hibernate.Session;
 import uk.ac.cam.echo.data.Conference;
+import uk.ac.cam.echo.data.Conversation;
 import uk.ac.cam.echo.data.resources.ConferenceResource;
 import uk.ac.cam.echo.server.HibernateUtil;
 import uk.ac.cam.echo.server.models.ConferenceModel;
@@ -11,11 +11,27 @@ import java.util.List;
 
 public class ConferenceResourceImpl implements ConferenceResource {
     public List<Conference> getAll() {
-        List<Conference> res = HibernateUtil.getTransaction().createCriteria(ConferenceModel.class).list();
-        Session s = HibernateUtil.getSession();
-        res.get(0).getConversationSet();
-        System.out.println(res.size());
-        return res;
+        return HibernateUtil.getTransaction().createCriteria(ConferenceModel.class).list();
+    }
+
+    @Override
+    public List<Conversation> search(String keyword, int n) {
+        throw  new UnsupportedOperationException("Not Implemented yet");
+    }
+
+    @Override
+    public List<Conversation> onlyTagSearch(String keyword, int n) {
+        throw  new UnsupportedOperationException("Not Implemented yet");
+    }
+
+    @Override
+    public List<Conversation> mostUsers(int n) {
+        throw  new UnsupportedOperationException("Not Implemented yet");
+    }
+
+    @Override
+    public List<Conversation> mostActiveRecently(int minutes, int n) {
+        throw  new UnsupportedOperationException("Not Implemented yet");
     }
 
     public Conference get(long id) {
