0f3153b3f5d5d6e5e65ed49704d381e5fa79364a
Petar Veličković <pv273@cam.ac.uk>
2014-02-04 01:34:03 +0000

    Added the Hunspell main file + LGPL licence.

---
+++ server/src/main/java/uk/ac/cam/echo/server/analysis/hunspell/HunspellMain.java
@@ -0,0 +1,87 @@
+package uk.ac.cam.echo.server.analysis.hunspell;
+
+import java.util.List;
+
+/**
+ * Simple testing and native build utility class, not useful in applications.
+ *
+ * The Hunspell java bindings are licensed under the same terms as Hunspell itself (GPL/LGPL/MPL tri-license),
+ * see the file COPYING.txt in the root of the distribution for the exact terms.
+ *
+ * @author Flemming Frandsen (flfr at stibo dot com)
+ * @author Hartmut Goebel (h dot goebel at crazy-compilers dot com)
+ *
+ *  Usage:  java -Droot=/usr/share/dicts -Dlang=de_DE HunspellMain
+ */
+
+public class HunspellMain {
+
+    private static void println(String msg) {
+        System.err.println(msg);
+    }
+    private static void print(String msg) {
+        System.err.print(msg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            if (args.length == 1 && args[0].equals("-libname")) {
+                //System.out.println(Hunspell.libName());
+
+            } else {
+
+                System.err.println("Loading Hunspell");
+                String dir = "/usr/share/hunspell";
+                if (System.getProperties().containsKey("root")) {
+                    dir = System.getProperty("root");
+                }
+
+                String language = "da_DK";
+                if (System.getProperties().containsKey("lang")) {
+                    language = System.getProperty("lang");
+                }
+
+                //Hunspell.Dictionary d = Hunspell.getInstance().getDictionary(dir+"/"+language);
+                System.err.println("Hunspell library and dictionary loaded");
+
+                String words[] = {"Test", "Hest", "guest", "ombudsmandshat", "ombudsman",
+                        "ymerfest", "g0r\u00f8ftegraver", "h\u00e6ngeplante", "garageport", "postbil", "huskop",
+                        "arne", "pladderballe", "Doctor", "Leo", "Lummerkrog",
+                        "Barnevognsbrand","barnehovedbekl\u00e6dning",
+                        "ymer", "drys", "ymerdrys",
+                        "\u00e6sel", "m\u00e6lk", "\u00e6selm\u00e6lk",
+                        "Brotbacken", "Pausbacken", "pausbackig", "Backenknochenbruch",
+                        "Donnerdampfschifffahrt",
+                        "Donnerdampfschifffahrtsgesellschaftskapit\u00e4n",
+                        "Messer", "Schleifer", "Messerschleifer",
+                        "muss", "mu\u00df"
+                };
+
+                for (int i=0;i<words.length;i++) {
+
+                    String word = words[i];
+                    /*if (d.misspelled(word)) {
+                        List<String> suggestions = d.suggest(word);
+                        print("misspelled: "+word);
+                        if (suggestions.isEmpty()) {
+                            print("\tNo suggestions.");
+                        } else {
+                            print("\tTry:");
+                            for (String s : suggestions) {
+                                print(" "+s);
+                            }
+                        }
+                        println("");
+                    } else {
+                        println("ok: "+word);
+                    }*/
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            println("Failed: "+e);
+        }
+    }
+}
+
