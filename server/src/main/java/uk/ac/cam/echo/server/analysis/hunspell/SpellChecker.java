7161b5d4c7b1c1d4026a648e770e2cd94a9890c7
Petar Veličković <pv273@cam.ac.uk>
2014-02-05 00:05:20 +0000

    Successfully added the HunspellBridJ reference.
    Also added a basic British English dictionary to the resources.

---
+++ server/src/main/java/uk/ac/cam/echo/server/analysis/hunspell/SpellChecker.java
@@ -0,0 +1,52 @@
+package uk.ac.cam.echo.server.analysis.hunspell;
+
+import com.atlascopco.hunspell.Hunspell;
+
+import java.util.List;
+
+/**
+ Author: Petar 'PetarV' Veličković
+
+ A class that connects to the Hunspell library
+ on behalf of the data analysis module
+ in order to simplify the communication.
+
+ Hunspell is released under the LGPL licence,
+ a copy of which is provided within this folder.
+*/
+public class SpellChecker
+{
+    private Hunspell speller;
+    private String dictionaryPath;
+    private String affixPath;
+
+    public SpellChecker(String dictionary, String affix)
+    {
+        this.speller = new Hunspell(dictionary, affix);
+        this.dictionaryPath = dictionary;
+        this.affixPath = affix;
+    }
+
+    private boolean check(String word)
+    {
+        return speller.spell(word);
+    }
+
+    public String correct(String word)
+    {
+        // Automatically take the first suggestion.
+        // If there are no suggestions, add word to dictionary.
+        if (check(word)) return word;
+        else
+        {
+            List<String> suggestions = speller.suggest(word);
+            if (suggestions.isEmpty())
+            {
+                speller.add(word);
+                return word;
+            }
+            return suggestions.get(0);
+        }
+    }
+
+}
