37b420bf5757265c39ed6887b5b8d363fa016c7f
Petar Veličković <pv273@cam.ac.uk>
2014-02-14 12:05:52 +0000

    Fixed error in HTTP request. Now call /recent-messages.

--- data/src/main/java/uk/ac/cam/echo/data/resources/MessageResource.java
+++ data/src/main/java/uk/ac/cam/echo/data/resources/MessageResource.java
@@ -14,6 +14,7 @@ public interface MessageResource extends RestResource<Message>{
     public Collection<Message> getAll();
 
     @GET
+    @Path("/recent-messages")
     public List<Message> getRecent(@QueryParam("num") int n);
 
     @GET
