776def2c147060ab0a292d83a40e8ff98877add4
Yojan Patel <yojanpatel@gmail.com>
2014-02-16 22:14:49 +0000

    updated service with notifications [ci skip]

--- EchoAndroid/src/main/java/uk/ac/cam/echo/services/EchoService.java
+++ EchoAndroid/src/main/java/uk/ac/cam/echo/services/EchoService.java
@@ -2,47 +2,101 @@ package uk.ac.cam.echo.services;
 
 import java.util.List;
 
+import uk.ac.cam.echo.activities.ConversationDetailActivity;
 import uk.ac.cam.echo.client.ClientApi;
 import uk.ac.cam.echo.data.Conference;
 import uk.ac.cam.echo.data.Conversation;
 import uk.ac.cam.echo.data.Message;
+import uk.ac.cam.echo.data.async.Handler;
+
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
-public class EchoService extends IntentService {
+public class EchoService extends Service {
+
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder binder = new LocalBinder();
 
 	private long conferenceId;
 	private long conversationId;
 	
 	private Conference conference;
 	private Conversation conversation;
-	
-	
+
 	private static ClientApi api;
-	
-	public EchoService(String name) {
-		super(name);
-		api = new ClientApi("http://echoconf.herokuapp.com/");
-	}
-	
-	public long getConferenceId() {
-		return conferenceId;
-	}
-	
+
+    private NotificationManager notificationManager;
+    private boolean notifEnabled;
+    private int numMessages;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        api = new ClientApi("http://echoconf.herokuapp.com/");
+        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notifEnabled = true;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public void toggleNotifs() { notifEnabled = !notifEnabled; }
+
+
+    private Notification.Builder getNotification(Message message) {
+
+        Conversation conversation = message.getConversation();
+
+        // Intent that is triggered from notification
+        Intent intent = new Intent(this, ConversationDetailActivity.class);
+        intent.putExtra("_id", conversation.getId());
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification.Builder notifBuilder = new Notification.Builder(this)
+                .setContentTitle("New message in " + conversation.getName())
+                .setContentText(message.getSender().getUsername() + ": " + message.getContents())
+                .setContentIntent(pIntent)
+                .setAutoCancel(true);
+
+        return notifBuilder;
+    }
+
+   public void listenToConversation(long id) {
+
+       if(conversationId != id) {
+           //TODO: unsubscribe conversationId
+
+           conversationId = id;
+           Handler<Message> handler = new Handler<Message>() {
+               @Override
+               public void handle(Message message) {
+                   Notification.Builder notifBuilder = getNotification(message);
+                   notifBuilder.setNumber(++numMessages);
+                   notificationManager.notify((int)message.getConversation().getId(),
+                           notifBuilder.build());
+               }
+           };
+           api.conversationResource.listenToMessages(id).subscribe(handler);
+       }
+   }
+
 	public long getConversationId() {
 		return conversationId;
-	}
-	
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
 	}
 
 	public void sendMessage(String msgContents) {
@@ -79,7 +133,17 @@ public class EchoService extends IntentService {
 		
 		return true;
 	}
-	
+
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        public EchoService getService() {
+            return EchoService.this;
+        }
+    }
 	
 
 }
