dcae021524e9d22b5e62eebc33a624dc0952cfbb
Alexandru Tache <hadesgames@gmail.com>
2014-02-18 01:49:07 +0000

    Missing setters for Message

--- data/src/main/java/uk/ac/cam/echo/data/Message.java
+++ data/src/main/java/uk/ac/cam/echo/data/Message.java
@@ -5,7 +5,9 @@ public interface Message extends Base
     public long getId();
     public long getTimeStamp();
     public User getSender();
+    public void setSender(User user);
     public Conversation getConversation();
+    public void setConversation(Conversation conversation);
     public String getContents();
     public void setContents(String contents);
 }
