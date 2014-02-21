2061fbcf4331c9042ce5645d4606ab7f0d94d766
Petar Veličković <pv273@cam.ac.uk>
2014-02-21 15:16:44 +0000

    New version of the user keyworder test.

--- server/src/test/java/uk/ac/cam/echo/test/analysis/UserKeyworderTest.java
+++ server/src/test/java/uk/ac/cam/echo/test/analysis/UserKeyworderTest.java
@@ -11,7 +11,9 @@ import uk.ac.cam.echo.server.models.ConversationModel;
 import uk.ac.cam.echo.server.models.TagModel;
 import uk.ac.cam.echo.server.models.UserModel;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  Author: Petar 'PetarV' Veličković
@@ -19,13 +21,24 @@ import java.util.Map;
 public class UserKeyworderTest
 {
     private User test_user;
-    private Conversation test_conversation;
+    private ConversationModel test_conversation;
+    private Set<Tag> test_tags;
 
     @Before
     public void setUp() throws Exception
     {
+        Tag t1 = new TagModel();
+        t1.setName("Tag XYZ");
+        Tag t2 = new TagModel();
+        t2.setName("ZYX gaT");
+
+        test_tags = new HashSet<Tag>();
+        test_tags.add(t1);
+        test_tags.add(t2);
+
         test_conversation = new ConversationModel();
         test_conversation.setName("Conversation XYZ");
+        test_conversation.setTags(test_tags);
 
         test_user = new UserModel();
         test_user.setFirstName("Petar");
@@ -53,7 +66,10 @@ public class UserKeyworderTest
         assert(keywords.get("engineer") == 1);
         assert(keywords.get("mdcs") == 1);
         assert(keywords.get("conversation") == 2);
-        assert(keywords.get("xyz") == 2);
+        assert(keywords.get("xyz") == 3);
+        assert(keywords.get("tag") == 1);
+        assert(keywords.get("gat") == 1);
+        assert(keywords.get("zyx") == 1);
 
         System.out.println(UserKeyworder.buildPersonalKeyword(test_user));
     }
